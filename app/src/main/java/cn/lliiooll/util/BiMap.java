aa256243a89c2515ed877961156af0d0eddd6004
lliioollcn <lliiooll@126.com>
2022-02-01 00:01:39 +0800

    add lliiooll utils overlayfs
    Signed-off-by: ACh Sulfate <xenonhydride@gmail.com>

---
+++ app/src/main/java/cn/lliiooll/util/BiMap.java
@@ -0,0 +1,30 @@
+/*
+ * QAuxiliary - An Xposed module for QQ/TIM
+ * Copyright (C) 2019-2022 qwq233@qwq2333.top
+ * https://github.com/cinit/QAuxiliary
+ *
+ * This software is non-free but opensource software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version and our eula as published
+ * by ferredoxin.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * and eula along with this software.  If not, see
+ * <https://www.gnu.org/licenses/>
+ * <https://github.com/cinit/QAuxiliary/blob/master/LICENSE.md>.
+ */
+
+package cn.lliiooll.util;
+
+import java.util.Map;
+
+public interface BiMap<K, V> extends Map<K, V> {
+
+    BiMap<V, K> getReverseMap();
+}
