0a1cf5f9a00f9d8469c7cb778634a29888492d88
ACh Sulfate <xenonhydride@gmail.com>
2022-02-11 21:07:07 +0800

    refactor: add MMKV as submodules
    Signed-off-by: ACh Sulfate <xenonhydride@gmail.com>

--- libs/mmkv/Android/src/main/java/com/tencent/mmkv/MMKVLogLevel.java
+++
@@ -1,9 +0,0 @@
-package com.tencent.mmkv;
-
-public enum MMKVLogLevel {
-    LevelDebug, // not available for release/production build
-    LevelInfo,  // default level
-    LevelWarning,
-    LevelError,
-    LevelNone // special level used to disable all log messages
-}
