0af4dbff40e34c8aa7ba439987692e76be4024ac
NextAlone <12210746+NextAlone@users.noreply.github.com>
2022-02-12 14:03:55 +0800

    build: simplify property reader

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,16 +1,17 @@
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import java.io.File
+import java.util.*
 
 object Version {
     const val kotlin = "1.5.31"
     const val ksp = "1.0.1"
     val java = JavaVersion.VERSION_11
 
-    const val defaultNdkVersion = "23.1.7779620"
-    const val defaultCMakeVersion = "3.18.1"
+    private const val defaultNdkVersion = "23.1.7779620"
+    private const val defaultCMakeVersion = "3.18.1"
 
-    fun getNdkVersion(project: Project): String? {
+    fun getNdkVersion(project: Project): String {
         val prop = getLocalProperty(project, "qauxv.override.ndk.version")
         val env = getEnvVariable("QAUXV_OVERRIDE_NDK_VERSION")
         if (!prop.isNullOrEmpty() && !env.isNullOrEmpty()) {
@@ -19,7 +20,7 @@ object Version {
         return prop ?: env ?: defaultNdkVersion
     }
 
-    fun getCMakeVersion(project: Project): String? {
+    fun getCMakeVersion(project: Project): String {
         val prop = getLocalProperty(project, "qauxv.override.cmake.version")
         val env = getEnvVariable("QAUXV_OVERRIDE_CMAKE_VERSION")
         if (!prop.isNullOrEmpty() && !env.isNullOrEmpty()) {
@@ -34,15 +35,9 @@ object Version {
         if (!localProp.exists()) {
             return null
         }
-        val content = localProp.readText(Charsets.UTF_8)
-        val lines = content.replace("\r", "").split("\n")
-        for (line in lines) {
-            val parts = line.split("=")
-            if (parts.size == 2 && parts[0].trim() == propertyName) {
-                return parts[1].trim()
-            }
-        }
-        return null
+        val localProperties = Properties()
+        localProperties.load(localProp.inputStream())
+        return localProperties.getProperty(propertyName, null)
     }
 
     private fun getEnvVariable(name: String): String? {
