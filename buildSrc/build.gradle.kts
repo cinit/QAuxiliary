8684799d3332f5a42d967c512970001554da279d
keta1 <ketal@qq.com>
2022-02-12 11:19:30 +0800

    gradle: update jvm kt

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     `kotlin-dsl`
 }
@@ -9,5 +11,16 @@ repositories {
 
     dependencies {
         implementation("org.eclipse.jgit:org.eclipse.jgit:5.10.0.202012080955-r")
+    }
+}
+
+java {
+    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "11"
     }
 }
