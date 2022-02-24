d66e515b237a58d72f2992bb874f731c01e17f82
keta1 <k@ketal.icu>
2022-02-24 22:22:19 +0800

    ui: add new icon
    Signed-off-by: keta1 <k@ketal.icu>

--- buildSrc/src/main/kotlin/ReplaceIcon.kt
+++ buildSrc/src/main/kotlin/ReplaceIcon.kt
@@ -1,34 +1,28 @@
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 import java.io.File
-import java.util.*
 
 abstract class ReplaceIcon : DefaultTask() {
     @TaskAction
     fun run() {
         val iconsDir = File(project.projectDir, "icons")
+        val drawableDir= File(project.projectDir, "src/main/res/drawable")
+        // delete old icons
+        drawableDir
+                .listFiles()
+                ?.filter { it.isFile && it.name.startsWith("icon.") }
+                ?.forEach(File::delete)
+
+        // copy new icons
         val iconFileDirs = listOf(
 //            File(iconsDir, "MiStyleIcons"),
-//            File(iconsDir, "classic"),
+            File(iconsDir, "classic"),
             File(iconsDir, "ChineseNewYearIcons")
         )
-        val fileCount = iconFileDirs.fold(0) { i: Int, file: File ->
-            i + (file.listFiles()?.size ?: 0)
-        }
-        var number = Random().nextInt(fileCount)
 
-        //for (aNumber in 0..fileCount) {
-        //var number = aNumber
-        var iconFile: File? = null
-        for (iconFileDir in iconFileDirs) {
-            if (number < iconFileDir.listFiles()!!.size) {
-                iconFile = iconFileDir.listFiles()!![number]
-                break
-            }
-            number -= iconFileDir.listFiles()!!.size
-        }
+        val iconFile: File? = iconFileDirs.random().listFiles()?.random()
+        val suffix = iconFile?.name?.substringAfterLast(".") ?: "png"
         println("Select Icon: $iconFile")
-        iconFile!!.copyTo(File(project.projectDir, "src/main/res/drawable/icon.png"), true)
-        //}
+        iconFile!!.copyTo(File(drawableDir, "icon.$suffix"), true)
     }
 }
