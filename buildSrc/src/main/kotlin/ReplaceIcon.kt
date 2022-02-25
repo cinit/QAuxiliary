b398a3e595e6912e58cb1669f82601b10b1b9c05
keta1 <k@ketal.icu>
2022-02-26 01:42:09 +0800

    ui: support adaptive icon
    Signed-off-by: keta1 <k@ketal.icu>

--- buildSrc/src/main/kotlin/ReplaceIcon.kt
+++ buildSrc/src/main/kotlin/ReplaceIcon.kt
@@ -7,22 +7,32 @@ abstract class ReplaceIcon : DefaultTask() {
     fun run() {
         val iconsDir = File(project.projectDir, "icons")
         val drawableDir= File(project.projectDir, "src/main/res/drawable")
-        // delete old icons
+        val adapter = File(project.projectDir, "src/main/res/drawable-anydpi-v26/icon.xml")
+
+        // delete old icons: all files starting with `icon`
         drawableDir
-                .listFiles()
-                ?.filter { it.isFile && it.name.startsWith("icon.") }
-                ?.forEach(File::delete)
+            .listFiles()
+            ?.filter { it.isFile && it.name.startsWith("icon") }
+            ?.forEach(File::delete)
+        if (adapter.exists()) adapter.delete()
 
         // copy new icons
         val iconFileDirs = listOf(
-//            File(iconsDir, "MiStyleIcons"),
             File(iconsDir, "classic"),
-            File(iconsDir, "ChineseNewYearIcons")
+            //File(iconsDir, "ChineseNewYearIcons")
         )
 
-        val iconFile: File? = iconFileDirs.random().listFiles()?.random()
-        val suffix = iconFile?.name?.substringAfterLast(".") ?: "png"
+        val iconFile = iconFileDirs.random().listFiles()!!.random()
         println("Select Icon: $iconFile")
-        iconFile!!.copyTo(File(drawableDir, "icon.$suffix"), true)
+        if (iconFile.isDirectory) {
+            // copy all files in the drawable directory
+            iconFile.listFiles()?.forEach {
+                val target = File(drawableDir, it.name)
+                it.copyTo(target, true)
+            }
+            File(iconsDir, "icon.xml").copyTo(adapter, true)
+        } else {
+            iconFile.copyTo(File(drawableDir, "icon.png"), true)
+        }
     }
 }
