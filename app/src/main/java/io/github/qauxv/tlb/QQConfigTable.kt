594cf9f6a29941ed20b8e318630bcffb0df48fd5
NextAlone <12210746+NextAlone@users.noreply.github.com>
2022-03-01 08:35:02 +0800

    fix: chat words count on QQ 8.8.80

--- app/src/main/java/io/github/qauxv/tlb/QQConfigTable.kt
+++ app/src/main/java/io/github/qauxv/tlb/QQConfigTable.kt
@@ -58,6 +58,7 @@ class QQConfigTable : ConfigTableInterface {
                     QQ_8_8_38 to "ncy",
                     QQ_8_8_50 to "nf4",
                     QQ_8_8_68 to "nm7",
+                    QQ_8_8_80 to "nmw",
             ),
 
             VasProfileAntiCrash::class.java.simpleName to mapOf(
