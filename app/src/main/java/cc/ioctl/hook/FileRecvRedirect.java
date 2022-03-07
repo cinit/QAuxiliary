60ad0b32d1657c5fbe70b21d77e7d40d645180ff
Hicores <hicores@163.com>
2022-03-07 09:08:03 +0800

    fix : make FileRecvRedirect available

--- app/src/main/java/cc/ioctl/hook/FileRecvRedirect.java
+++ app/src/main/java/cc/ioctl/hook/FileRecvRedirect.java
@@ -23,10 +23,18 @@ package cc.ioctl.hook;
 
 import android.app.Activity;
 import android.os.Environment;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import cc.ioctl.util.HostInfo;
+import com.afollestad.materialdialogs.MaterialDialog;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import io.github.qauxv.SyncUtils;
 import io.github.qauxv.base.IUiItemAgent;
 import io.github.qauxv.base.annotation.FunctionHookEntry;
 import io.github.qauxv.base.annotation.UiItemAgentEntry;
@@ -35,9 +43,11 @@ import io.github.qauxv.config.ConfigManager;
 import io.github.qauxv.dsl.FunctionEntryRouter.Locations.Auxiliary;
 import io.github.qauxv.hook.CommonConfigFunctionHook;
 import io.github.qauxv.util.DexKit;
+import io.github.qauxv.util.Initiator;
 import io.github.qauxv.util.Log;
 import io.github.qauxv.util.QQVersion;
 import io.github.qauxv.util.Toasts;
+import java.io.File;
 import java.lang.reflect.Field;
 import kotlin.Unit;
 import kotlin.jvm.functions.Function3;
@@ -53,7 +63,7 @@ public class FileRecvRedirect extends CommonConfigFunctionHook {
     private Field TARGET_FIELD = null;
 
     private FileRecvRedirect() {
-        super(new int[]{DexKit.C_APP_CONSTANTS});
+        super(SyncUtils.PROC_ANY);
     }
 
     @NonNull
@@ -84,13 +94,52 @@ public class FileRecvRedirect extends CommonConfigFunctionHook {
     @Override
     public Function3<IUiItemAgent, Activity, View, Unit> getOnUiItemClickListener() {
         return (agent, activity, view) -> {
-            Toasts.show(activity,"坏了, 没修");
+            LinearLayout mRoot = new LinearLayout(activity);
+            mRoot.setOrientation(LinearLayout.VERTICAL);
+
+            CheckBox start = new CheckBox(activity);
+            start.setText("开启下载重定向");
+            start.setChecked(isEnabled());
+
+            mRoot.addView(start);
+
+            EditText PathSet = new EditText(activity);
+            PathSet.setText(getRedirectPath());
+            mRoot.addView(PathSet);
+            PathSet.setVisibility(start.isChecked() ? View.VISIBLE :View.GONE);
+
+            start.setOnCheckedChangeListener((buttonView, isChecked) -> PathSet.setVisibility(isChecked ? View.VISIBLE :View.GONE));
+
+
+
+            MaterialDialog dialog = new MaterialDialog(activity,MaterialDialog.getDEFAULT_BEHAVIOR());
+            dialog.title(null,"下载文件重定向");
+            dialog.positiveButton(null, "保存", materialDialog -> {
+                String Path = PathSet.getText().toString();
+                if (!CheckPathIsAvailable(Path)){
+                    Toasts.show(activity,"目录可能无效");
+                    return null;
+                }
+                setEnabled(start.isChecked());
+                if (start.isChecked()){
+                    setRedirectPathAndEnable(PathSet.getText().toString());
+                }
+                Toasts.show(activity,"已保存,请重启QQ");
+                return null;
+            });
+            dialog.negativeButton(null, "取消", materialDialog -> null);
+            dialog.getView().contentLayout.setCustomView(null);
+            dialog.getView().contentLayout.addCustomView(null,mRoot,false,false,false);
+
+            dialog.show();
+
             return Unit.INSTANCE;
         };
     }
-
+    private static String CacheDefPath;
     @Override
     public boolean initOnce() throws Exception {
+        CacheDefPath = getDefaultPath();
         String redirectPath = getRedirectPath();
         if (redirectPath != null) {
             inited = doSetPath(redirectPath);
@@ -101,26 +150,52 @@ public class FileRecvRedirect extends CommonConfigFunctionHook {
     }
 
     private boolean doSetPath(String str) {
-        Field[] fields = DexKit.doFindClass(DexKit.C_APP_CONSTANTS).getFields();
+
         try {
-            if (TARGET_FIELD == null) {
-                for (Field field : fields) {
-                    field.setAccessible(true);
-                    Object value = field.get(null);
-                    String path = String.valueOf(value);
-                    if (path.toLowerCase().endsWith("file_recv/")) {
-                        TARGET_FIELD = field;
-                        break;
+            if (HostInfo.requireMinQQVersion(QQVersion.QQ_8_2_8)){
+                if(!inited){
+                    XposedHelpers.findAndHookMethod(Initiator.load("com.tencent.mobileqq.vfs.VFSAssistantUtils"), "getSDKPrivatePath", String.class,
+                            new XC_MethodHook() {
+                                @Override
+                                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                                    super.afterHookedMethod(param);
+                                    String getResult = (String) param.getResult();
+                                    File checkAvailable = new File(getResult);
+                                    if (checkAvailable.exists() && checkAvailable.isFile())return;//如果文件存在则不处理
+                                    if (isEnabled() && getResult.startsWith(CacheDefPath)){
+                                        //在QQ尝试获取文件目录时进行替换
+                                        param.setResult(getRedirectPath()+getResult.substring(CacheDefPath.length()));
+                                    }
+                                }
+                            });
+                }
+
+            }else {
+                Field[] fields = DexKit.doFindClass(DexKit.C_APP_CONSTANTS).getFields();
+                if (TARGET_FIELD == null) {
+                    for (Field field : fields) {
+                        field.setAccessible(true);
+                        Object value = field.get(null);
+                        String path = String.valueOf(value);
+                        if (path.toLowerCase().endsWith("file_recv/")) {
+                            TARGET_FIELD = field;
+                            break;
+                        }
                     }
                 }
+                TARGET_FIELD.setAccessible(true);
+                TARGET_FIELD.set(null, str);
             }
-            TARGET_FIELD.setAccessible(true);
-            TARGET_FIELD.set(null, str);
             return true;
         } catch (Exception e) {
             Log.e(e);
             return false;
         }
+    }
+    public boolean CheckPathIsAvailable(String Path){
+        File f = new File(Path);
+        f = f.getParentFile();
+        return f.listFiles() != null;
     }
 
     public String getDefaultPath() {
@@ -130,17 +205,22 @@ public class FileRecvRedirect extends CommonConfigFunctionHook {
         } else {
             if (HostInfo.requireMinQQVersion(QQVersion.QQ_8_2_8)) {
                 return HostInfo.getApplication()
-                        .getExternalFilesDir(null) + "/Tencent/QQfile_recv/";
+                        .getExternalFilesDir(null).getParent() + "/Tencent/QQfile_recv";
             } else {
                 return Environment.getExternalStorageDirectory().getAbsolutePath()
-                        + "/Tencent/QQfile_recv/";
+                        + "/Tencent/QQfile_recv";
             }
         }
     }
 
     @Nullable
     public String getRedirectPath() {
-        return ConfigManager.getDefaultConfig().getString(ConfigItems.qn_file_recv_redirect_path);
+        String path = ConfigManager.getDefaultConfig().getString(ConfigItems.qn_file_recv_redirect_path);
+        if (TextUtils.isEmpty(path)){
+            path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)+"/MobileQQ/";
+        }
+        if (path.endsWith("/"))return path.substring(0,path.length()-1);
+        return path;
     }
 
     public void setRedirectPathAndEnable(String path) {
@@ -162,7 +242,7 @@ public class FileRecvRedirect extends CommonConfigFunctionHook {
 
     @Override
     public boolean isEnabled() {
-        return false && ConfigManager.getDefaultConfig().getBooleanOrFalse(ConfigItems.qn_file_recv_redirect_enable);
+        return ConfigManager.getDefaultConfig().getBooleanOrFalse(ConfigItems.qn_file_recv_redirect_enable);
     }
 
     /**
