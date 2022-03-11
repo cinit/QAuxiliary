b4be1070ae43e11e3f217257f06890c0fa335d17
keta1 <k@ketal.icu>
2022-03-11 17:03:32 +0800

    update AGP 7.1.2
    Signed-off-by: keta1 <k@ketal.icu>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "7.1.1" apply false
-    id("com.android.library") version "7.1.1" apply false
+    id("com.android.application") version "7.1.2" apply false
+    id("com.android.library") version "7.1.2" apply false
     id("org.jetbrains.kotlin.android") version Version.kotlin apply false
     // id("org.jetbrains.compose") version "1.0.0" apply false
 }
