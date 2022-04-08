9f22d12127e9a3eb66f6da944c58517d18c0ad38
SakuraKooi <sakurakoi993519867@gmail.com>
2022-04-08 14:02:53 +0800

    remove: Hide eula & disable blacklist & unlock developer options

--- app/src/main/java/io/github/qauxv/util/LicenseStatus.kt
+++ app/src/main/java/io/github/qauxv/util/LicenseStatus.kt
@@ -22,112 +22,34 @@
 
 package io.github.qauxv.util
 
-import io.github.qauxv.SyncUtils
-import io.github.qauxv.bridge.AppRuntimeHelper
-import io.github.qauxv.config.ConfigManager
-import io.github.qauxv.remote.TransactionHelper.getUserStatus
-import io.github.qauxv.util.data.UserStatusConst
-import java.util.Date
-
 object LicenseStatus {
 
     @JvmField
     val sDisableCommonHooks: Boolean = isBlacklisted()
 
-    private const val qn_eula_status = "qa_eula_status" //typo, ignore it
-    private const val qn_user_auth_status = "qn_user_auth_status"
-    private const val qn_user_auth_last_update = "qn_user_auth_last_update"
     const val CURRENT_EULA_VERSION = 9
 
     @JvmStatic
-    fun getEulaStatus(): Int {
-        val cfg = ConfigManager.getDefaultConfig()
-        return cfg.getIntOrDefault(qn_eula_status, 0)
-    }
-
-    @JvmStatic
     fun setEulaStatus(status: Int) {
-        val cfg = ConfigManager.getDefaultConfig()
-        cfg.putInt(qn_eula_status, status)
-        cfg.save()
     }
 
     @JvmStatic
     fun hasEulaUpdated(): Boolean {
-        val s = getEulaStatus()
-        return s != 0 && s != CURRENT_EULA_VERSION
+        return false;
     }
 
     @JvmStatic
     fun hasUserAcceptEula(): Boolean {
-        return getEulaStatus() == CURRENT_EULA_VERSION
-    }
-
-    @JvmStatic
-    fun setUserCurrentStatus() {
-        SyncUtils.async {
-            val cfg = ConfigManager.getDefaultConfig()
-            val currentStatus = getUserStatus(AppRuntimeHelper.getLongAccountUin())
-            // 如果获取不到就放弃更新状态
-            if (currentStatus == UserStatusConst.notExist) {
-                return@async
-            }
-            Log.i("User Current Status: $currentStatus")
-            cfg.putInt(qn_user_auth_status, currentStatus)
-            cfg.putLong(qn_user_auth_last_update, System.currentTimeMillis())
-            cfg.save()
-            Log.i("User Current Status in ConfigManager: " + cfg.getIntOrDefault(qn_user_auth_status, -1))
-            Log.i("User Status Last Update: " + Date(cfg.getLongOrDefault(qn_user_auth_last_update, System.currentTimeMillis())))
-        }
-    }
-
-
-    @JvmStatic
-    fun isInsider(): Boolean {
-        val cfg = ConfigManager.getDefaultConfig()
-        var currentStatus = cfg.getIntOrDefault(qn_user_auth_status, -1)
-        if (currentStatus == UserStatusConst.notExist) {
-            setUserCurrentStatus()
-            currentStatus = cfg.getIntOrDefault(qn_user_auth_status, -1)
-        }
-        val lastUpdate = cfg.getLongOrDefault(qn_user_auth_last_update, System.currentTimeMillis())
-        if (lastUpdate >= lastUpdate + 30 * 60 * 1000) {
-            setUserCurrentStatus()
-        }
-        return currentStatus == UserStatusConst.developer
+        return true;
     }
 
     @JvmStatic
     fun isBlacklisted(): Boolean {
-        val cfg = ConfigManager.getDefaultConfig()
-        var currentStatus = cfg.getIntOrDefault(qn_user_auth_status, -1)
-        if (currentStatus == UserStatusConst.notExist) {
-            setUserCurrentStatus()
-            currentStatus = cfg.getIntOrDefault(qn_user_auth_status, -1)
-        }
-        val lastUpdate = cfg.getLongOrDefault(qn_user_auth_last_update, System.currentTimeMillis())
-        if (lastUpdate >= lastUpdate + 30 * 60 * 1000) {
-            setUserCurrentStatus()
-        }
-        return currentStatus == UserStatusConst.blacklisted
+        return false;
     }
 
     @JvmStatic
     fun isWhitelisted(): Boolean {
-        if (isInsider())
-            return true
-        val cfg = ConfigManager.getDefaultConfig()
-        var currentStatus = cfg.getIntOrDefault(qn_user_auth_status, -1)
-        if (currentStatus == UserStatusConst.notExist) {
-            setUserCurrentStatus()
-            currentStatus = cfg.getIntOrDefault(qn_user_auth_status, -1)
-        }
-        val lastUpdate = cfg.getLongOrDefault(qn_user_auth_last_update, System.currentTimeMillis())
-        if (lastUpdate >= lastUpdate + 30 * 60 * 1000) {
-            setUserCurrentStatus()
-        }
-        return currentStatus == UserStatusConst.whitelisted
+        return true;
     }
-
-
 }
