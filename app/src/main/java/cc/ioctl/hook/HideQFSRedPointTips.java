ac42517e7c5594bab126d91a1f1df8523c1807f2
ACh Sulfate <xenonhydride@gmail.com>
2022-04-17 21:26:43 +0800

    feat: add hide QFS red point tips

---
+++ app/src/main/java/cc/ioctl/hook/HideQFSRedPointTips.java
@@ -0,0 +1,87 @@
+/*
+ * QAuxiliary - An Xposed module for QQ/TIM
+ * Copyright (C) 2019-2022 qwq233@qwq2333.top
+ * https://github.com/cinit/QAuxiliary
+ *
+ * This software is non-free but opensource software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version and our eula as published
+ * by QAuxiliary contributors.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * and eula along with this software.  If not, see
+ * <https://www.gnu.org/licenses/>
+ * <https://github.com/cinit/QAuxiliary/blob/master/LICENSE.md>.
+ */
+
+package cc.ioctl.hook;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import cc.ioctl.util.HookUtils;
+import io.github.qauxv.SyncUtils;
+import io.github.qauxv.base.annotation.FunctionHookEntry;
+import io.github.qauxv.base.annotation.UiItemAgentEntry;
+import io.github.qauxv.dsl.FunctionEntryRouter;
+import io.github.qauxv.hook.CommonSwitchFunctionHook;
+import io.github.qauxv.util.HostInfo;
+import io.github.qauxv.util.Initiator;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Objects;
+
+@UiItemAgentEntry
+@FunctionHookEntry
+public class HideQFSRedPointTips extends CommonSwitchFunctionHook {
+
+    public static final HideQFSRedPointTips INSTANCE = new HideQFSRedPointTips();
+
+    private HideQFSRedPointTips() {
+        super(SyncUtils.PROC_MAIN);
+    }
+
+    @NonNull
+    @Override
+    public String[] getUiItemLocation() {
+        return FunctionEntryRouter.Locations.Simplify.MAIN_UI_TITLE;
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return "屏蔽好友推过";
+    }
+
+    @Nullable
+    @Override
+    public String getDescription() {
+        return "屏蔽消息列表主页左上角头像旁的好友推荐气泡";
+    }
+
+    @Override
+    protected boolean initOnce() throws Exception {
+        Class<?> kQFSRedPointTipsHelper = Initiator.loadClass("com.tencent.mobileqq.activity.qcircle.utils.QFSRedPointTipsHelper");
+        Method showTips = null;
+        for (Method method : kQFSRedPointTipsHelper.getDeclaredMethods()) {
+            if (method.getName().equals("a") && method.getParameterTypes().length == 3
+                    && !Modifier.isStatic(method.getModifiers())) {
+                showTips = method;
+                break;
+            }
+        }
+        Objects.requireNonNull(showTips, "QFSRedPointTipsHelper.showTips not found");
+        HookUtils.hookBeforeIfEnabled(this, showTips, p -> p.setResult(null));
+        return true;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return !HostInfo.isTim();
+    }
+}
