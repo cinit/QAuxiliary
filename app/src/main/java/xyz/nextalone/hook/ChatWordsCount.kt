146936e8f6ac4482fc650227a92b3469d5e4aeb2
Next Alone <12210746+NextAlone@users.noreply.github.com>
2022-06-04 16:42:03 +0800

    fix: ChatWordsCount resume

--- app/src/main/java/xyz/nextalone/hook/ChatWordsCount.kt
+++ app/src/main/java/xyz/nextalone/hook/ChatWordsCount.kt
@@ -113,12 +113,10 @@ object ChatWordsCount : CommonConfigFunctionHook("na_chat_words_count_kt", intAr
                 val viewGroup = it.args[2] as ViewGroup
                 updateChatWordView(viewGroup)
             }
-        } else {
-            // for older version
-            DexKit.doFindMethod(DexKit.N_QQSettingMe_onResume)?.hookAfter(this) {
-                val viewGroup = it.thisObject.get("a", ViewGroup::class.java) as ViewGroup
-                updateChatWordView(viewGroup)
-            }
+        }
+        DexKit.doFindMethod(DexKit.N_QQSettingMe_onResume)?.hookAfter(this) {
+            val viewGroup = it.thisObject.get(ViewGroup::class.java) as ViewGroup
+            updateChatWordView(viewGroup)
         }
         DexKit.doFindMethod(DexKit.N_ChatActivityFacade_sendMsgButton)?.hookAfter(this)
         {
