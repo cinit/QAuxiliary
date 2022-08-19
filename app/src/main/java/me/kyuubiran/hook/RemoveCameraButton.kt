ff351ed698d6b3ecaf88255497266727a7b75d03
lliioollcn <lliiooll@126.com>
2022-08-19 22:02:29 +0800

    fix: RemoveCameraButton on 8.9.5

--- app/src/main/java/me/kyuubiran/hook/RemoveCameraButton.kt
+++ app/src/main/java/me/kyuubiran/hook/RemoveCameraButton.kt
@@ -47,14 +47,15 @@ object RemoveCameraButton : CommonSwitchFunctionHook("kr_disable_camera_button")
         }.hookBefore {
             if (!isEnabled) return@hookBefore; it.result = null
         }
+
         findMethod(Initiator._ConversationTitleBtnCtrl()) {
-            val methodName = when {
-                requireMinQQVersion(QQVersion.QQ_8_8_93) -> "E"
-                requireMinQQVersion(QQVersion.QQ_8_8_93) -> "F"
-                else -> "a"
-            }
-            name == methodName
-                && returnType == Void.TYPE && parameterTypes.isEmpty()
+            val nameEqual = if (requireMinQQVersion(QQVersion.QQ_8_8_93))
+                (name == "E" || name == "F")
+            else
+                name == "a"
+
+            nameEqual && returnType == Void.TYPE && parameterTypes.isEmpty()
+
         }.hookBefore {
             if (!isEnabled) return@hookBefore; it.result = null
         }
