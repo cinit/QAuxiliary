d31c19bdb25f80124cdf0e22c21a01239ea1de1c
keta1 <k@ketal.icu>
2022-10-13 11:07:32 +0800

    fix: use apkPath create DexKitBridge
    Signed-off-by: keta1 <k@ketal.icu>

--- app/src/main/java/io/github/qauxv/util/dexkit/impl/DexKitDeobfs.kt
+++ app/src/main/java/io/github/qauxv/util/dexkit/impl/DexKitDeobfs.kt
@@ -22,8 +22,6 @@
 
 package io.github.qauxv.util.dexkit.impl
 
-import cc.ioctl.util.findDexClassLoader
-import io.github.qauxv.util.Initiator
 import io.github.qauxv.util.Log
 import io.github.qauxv.util.dexkit.DexDeobfsBackend
 import io.github.qauxv.util.dexkit.DexKit
@@ -31,6 +29,7 @@ import io.github.qauxv.util.dexkit.DexKitTarget
 import io.github.qauxv.util.dexkit.DexMethodDescriptor
 import io.github.qauxv.util.dexkit.name
 import io.github.qauxv.util.dexkit.valueOf
+import io.github.qauxv.util.hostInfo
 import io.luckypray.dexkit.DexKitBridge
 import io.luckypray.dexkit.descriptor.member.DexMethodDescriptor as MethodDescriptor
 import java.util.concurrent.locks.Lock
@@ -155,8 +154,8 @@ class DexKitDeobfs private constructor(
             object : SharedRefCountResourceImpl<DexKitBridge>() {
                 override fun openResourceInternal(): DexKitBridge {
                     Log.d("open resource: DexKit")
-                    val dexClassLoader: ClassLoader = Initiator.getHostClassLoader().findDexClassLoader()!!
-                    return DexKitBridge.create(dexClassLoader)!!
+                    val apkPath = hostInfo.application.applicationInfo.sourceDir
+                    return DexKitBridge.create(apkPath)!!
                 }
 
                 override fun closeResourceInternal(res: DexKitBridge) {
