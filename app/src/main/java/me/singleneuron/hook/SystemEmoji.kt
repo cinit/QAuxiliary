bb5129b2a26cd8cd808ed3c67da6015f43514183
Cryolitia <cryolitia@gmail.com>
2022-12-14 20:36:05 +0800

    Add SystemEmoji
    Signed-off-by: Cryolitia <Cryolitia@gmail.com>

---
+++ app/src/main/java/me/singleneuron/hook/SystemEmoji.kt
@@ -0,0 +1,58 @@
+/*
+ * QAuxiliary - An Xposed module for QQ/TIM
+ * Copyright (C) 2019-2022 qwq233@qwq2333.top
+ * https://github.com/cinit/QAuxiliary
+ *
+ * This software is non-free but opensource software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version and our eula as published
+ * by QAuxiliary contributors.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * and eula along with this software.  If not, see
+ * <https://www.gnu.org/licenses/>
+ * <https://github.com/cinit/QAuxiliary/blob/master/LICENSE.md>.
+ */
+
+package me.singleneuron.hook
+
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedHelpers
+import io.github.qauxv.base.annotation.FunctionHookEntry
+import io.github.qauxv.base.annotation.UiItemAgentEntry
+import io.github.qauxv.dsl.FunctionEntryRouter
+import io.github.qauxv.hook.CommonSwitchFunctionHook
+import xyz.nextalone.util.clazz
+import xyz.nextalone.util.throwOrTrue
+
+
+@UiItemAgentEntry
+@FunctionHookEntry
+object SystemEmoji : CommonSwitchFunctionHook() {
+
+    override val name: String = "强制使用系统Emoji"
+
+    override val uiItemLocation: Array<String> = FunctionEntryRouter.Locations.Simplify.UI_CHAT_MSG
+
+    override fun initOnce(): Boolean {
+        return throwOrTrue {
+            val r: XC_MethodHook = object : XC_MethodHook() {
+                @Throws(Throwable::class)
+                override fun beforeHookedMethod(param: MethodHookParam) {
+                    // this will be called before the clock was updated by the original
+                    // method
+                    param.result = -1
+                }
+            }
+            val loadClass: Class<*>? = "com.tencent.mobileqq.text.EmotcationConstants".clazz
+            XposedHelpers.findAndHookMethod(loadClass, "getSingleEmoji", Integer.TYPE, r)
+            XposedHelpers.findAndHookMethod(loadClass, "getDoubleEmoji", Integer.TYPE, Integer.TYPE, r)
+        }
+    }
+}
