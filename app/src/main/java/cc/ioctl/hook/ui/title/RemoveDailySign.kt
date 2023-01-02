dcdbed5b62a8ddaf9c033c94c2c0a37456b6d2ea
ACh Sulfate <xenonhydride@gmail.com>
2023-01-02 21:04:23 +0800

    fix: RemoveDailySign on 8.9.28
    Signed-off-by: ACh Sulfate <xenonhydride@gmail.com>

--- app/src/main/java/cc/ioctl/hook/ui/title/RemoveDailySign.kt
+++ app/src/main/java/cc/ioctl/hook/ui/title/RemoveDailySign.kt
@@ -22,22 +22,20 @@
 package cc.ioctl.hook.ui.title
 
 import android.widget.LinearLayout
+import cc.ioctl.util.HookUtils
 import cc.ioctl.util.HostInfo
 import com.github.kyuubiran.ezxhelper.utils.getObjectAs
 import com.github.kyuubiran.ezxhelper.utils.setViewZeroSize
-import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
 import io.github.qauxv.base.annotation.FunctionHookEntry
 import io.github.qauxv.base.annotation.UiItemAgentEntry
 import io.github.qauxv.dsl.FunctionEntryRouter
 import io.github.qauxv.hook.CommonSwitchFunctionHook
 import io.github.qauxv.util.Initiator.loadClass
-import io.github.qauxv.util.LicenseStatus
 import io.github.qauxv.util.QQVersion
 import io.github.qauxv.util.hostInfo
 import io.github.qauxv.util.isTim
 import io.github.qauxv.util.requireMinQQVersion
-import xyz.nextalone.util.throwOrTrue
 
 @FunctionHookEntry
 @UiItemAgentEntry
@@ -48,30 +46,28 @@ object RemoveDailySign : CommonSwitchFunctionHook("kr_remove_daily_sign") {
 
     override val isAvailable: Boolean get() = !isTim()
 
-    override fun initOnce() = throwOrTrue {
-        XposedBridge.hookAllConstructors(
-            loadClass("com.tencent.mobileqq.activity.QQSettingMe"),
-            object : XC_MethodHook() {
-                override fun afterHookedMethod(param: MethodHookParam) {
-                    if (LicenseStatus.sDisableCommonHooks) return
-                    if (!isEnabled) return
-                    if (hostInfo.packageName != HostInfo.PACKAGE_NAME_QQ) return
-                    try {
-                        // em_drawer_sign_up
-                        val dailySignName = when {
-                            requireMinQQVersion(QQVersion.QQ_8_9_3) -> "d0"
-                            requireMinQQVersion(QQVersion.QQ_8_8_93) -> "c0"
-                            requireMinQQVersion(QQVersion.QQ_8_8_17) -> "O"
-                            requireMinQQVersion(QQVersion.QQ_8_8_11) -> "N"
-                            hostInfo.versionCode == QQVersion.QQ_8_6_0 -> "b"
-                            else -> "a"
-                        }
-                        param.thisObject.getObjectAs<LinearLayout>(dailySignName, LinearLayout::class.java)
-                            .setViewZeroSize()
-                    } catch (e: Throwable) {
-                        traceError(e)
-                    }
-                }
-            })
+    override fun initOnce(): Boolean {
+        if (hostInfo.packageName != HostInfo.PACKAGE_NAME_QQ) return false
+        val callback = HookUtils.afterIfEnabled(this) { param ->
+            // em_drawer_sign_up
+            val dailySignName = when {
+                requireMinQQVersion(QQVersion.QQ_8_9_28) -> "i0"
+                requireMinQQVersion(QQVersion.QQ_8_9_25) -> "h0"
+                // gap
+                requireMinQQVersion(QQVersion.QQ_8_9_3) -> "d0"
+                requireMinQQVersion(QQVersion.QQ_8_8_93) -> "c0"
+                requireMinQQVersion(QQVersion.QQ_8_8_17) -> "O"
+                requireMinQQVersion(QQVersion.QQ_8_8_11) -> "N"
+                hostInfo.versionCode == QQVersion.QQ_8_6_0 -> "b"
+                else -> "a"
+            }
+            param.thisObject.getObjectAs<LinearLayout>(dailySignName, LinearLayout::class.java).setViewZeroSize()
+        }
+        if (requireMinQQVersion(QQVersion.QQ_8_9_25)) {
+            XposedBridge.hookAllConstructors(loadClass("com.tencent.mobileqq.activity.QQSettingMeView"), callback)
+        } else {
+            XposedBridge.hookAllConstructors(loadClass("com.tencent.mobileqq.activity.QQSettingMe"), callback)
+        }
+        return true
     }
 }
