cacf69de7a4afc298d0ec448fcbb9162baf330c3
o0kam1 <duzhaokun2@outlook.com>
2023-01-05 21:30:40 +0800

    send tts use silk

--- app/src/main/java/io/github/duzhaokun123/hook/SendTTSHook.kt
+++ app/src/main/java/io/github/duzhaokun123/hook/SendTTSHook.kt
@@ -24,6 +24,7 @@ package io.github.duzhaokun123.hook
 
 import android.content.Context
 import android.media.MediaExtractor
+import android.media.MediaFormat
 import android.os.Parcelable
 import android.speech.tts.TextToSpeech
 import android.speech.tts.UtteranceProgressListener
@@ -41,13 +42,13 @@ import io.github.qauxv.dsl.FunctionEntryRouter
 import io.github.qauxv.router.decorator.BaseSwitchFunctionDecorator
 import io.github.qauxv.router.decorator.IInputButtonDecorator
 import io.github.qauxv.router.dispacher.InputButtonHookDispatcher
-import io.github.qauxv.step.Step
 import io.github.qauxv.ui.CommonContextWrapper
 import io.github.qauxv.util.SyncUtils
 import io.github.qauxv.util.Toasts
 import io.github.qauxv.util.dexkit.CArkAppItemBubbleBuilder
 import io.github.qauxv.util.dexkit.CFaceDe
 import io.github.qauxv.util.dexkit.NBaseChatPie_init
+import io.github.qauxv.util.ptt.SilkEncodeUtils
 import mqq.app.AppRuntime
 import java.io.File
 import java.util.Locale
@@ -65,11 +66,6 @@ object SendTTSHook :
     ), IInputButtonDecorator {
 
     const val MI_TTS = "com.xiaomi.mibrain.speech"
-
-    //TODO: mp3 to silk
-//    lateinit var voiceRedPacketHelperImpl: Any
-//    lateinit var getRecorderParam: Method
-//    var recorderParam: Any? = null
 
     override val name: String
         get() = "文字转语音发送 (使用系统 TTS)"
@@ -98,8 +94,6 @@ object SendTTSHook :
             }
         }
         TTS.init(HostInfo.getApplication())
-//        voiceRedPacketHelperImpl = Initiator._VoiceRedPacketHelperImpl().findMethod { isStatic && name == "getInstance" }.invoke(null)!!
-//        getRecorderParam = Initiator._RecordParams().findMethod { isStatic && returnType == Initiator._RecorderParam() }
         return true
     }
 
@@ -108,7 +102,6 @@ object SendTTSHook :
 
     override fun onFunBtnLongClick(text: String, session: Parcelable, input: EditText, sendBtn: View, ctx1: Context, qqApp: AppRuntime): Boolean {
         if (isEnabled.not()) return false
-//        if (recorderParam == null) recorderParam = getRecorderParam(null, qqApp, true)
         val wc = CommonContextWrapper.createAppCompatContext(ctx1)
         val lines = text.split("\n")
         val line1 = lines.getOrNull(0) ?: ""
@@ -123,10 +116,11 @@ object SendTTSHook :
         val language = line1.substring(4).trim()
         if (language.isNotBlank()) {
             TTS.instance.language = Locale.forLanguageTag(language)
+        } else {
+            TTS.instance.language = Locale.getDefault()
         }
         val toSend = lines.slice(1 until lines.size).joinToString("\n")
         val mp3 = File(wc.externalCacheDir, "send_tts/mp3")
-//        val silk = File(wc.externalCacheDir, "send_tts/silk")
         mp3.parentFile!!.mkdirs()
         var tryCount = 0
         fun trySend(retry: Boolean = false) {
@@ -158,37 +152,32 @@ object SendTTSHook :
                                 trySend()
                             }
                         }.onSuccess {
-                            it.release()
-                            SyncUtils.runOnUiThread {
-                                val time = TimeFormat.format1.format(System.currentTimeMillis())
-                                val save = File(wc.externalCacheDir!!, "../Tencent/MobileQQ/tts/$time.mp3")
-                                mp3.copyTo(save)
-                                ChatActivityFacade.sendPttMessage(qqApp, session, save.absolutePath)
-                                input.setText("")
-                                Toasts.success(wc, "发送成功 (tryCount=$tryCount)")
-//                    runCatching {
-//                        val r = mp3ToSilk(mp3.absolutePath, silk.absolutePath)
-//                        if (r) {
-//                            val time = TimeFormat.format1.format(System.currentTimeMillis())
-//                            outFile = File(wc.externalCacheDir!!, "../Tencent/MobileQQ/569200079/ptt/${time.substring(0,6)}/${time.substring(6, 8)}/stream_${time.substring(0,16)}.slk")
-//                            outFile!!.parentFile!!.mkdirs()
-//                            silk.copyTo(outFile!!)
-//                        }
-//                        d.cancel()
-//                        if (!r) throw RuntimeException("mp3ToSilk failed (或许再试一下就好了")
-//                    }.onFailure {
-//                        SyncUtils.runOnUiThread {
-//                            AlertDialog.Builder(wc)
-//                                .setTitle(it.message ?: "未知错误")
-//                                .setMessage(it.stackTraceToString())
-//                                .show()
-//                        }
-//                    }.onSuccess {
-//                        SyncUtils.runOnUiThread {
-//                            ChatActivityFacade.sendPttMessage(qqApp, session, mp3.absolutePath)
-//                            input.setText("")
-//                        }
-//                    }
+                            val time = TimeFormat.format1.format(System.currentTimeMillis())
+                            val save = File(wc.externalCacheDir!!, "../Tencent/MobileQQ/tts/$time.silk")
+                            val trackFormat = it.getTrackFormat(0)
+                            runCatching {
+                                SilkEncodeUtils.nativePcm16leToSilkSS(
+                                    mp3.absolutePath,
+                                    save.absolutePath,
+                                    trackFormat.getInteger(MediaFormat.KEY_SAMPLE_RATE),
+                                    24000,
+                                    (trackFormat.getInteger(MediaFormat.KEY_SAMPLE_RATE) * 20) / 1000,
+                                    true
+                                )
+                                it.release()
+                            }.onFailure {
+                                SyncUtils.runOnUiThread {
+                                    AlertDialog.Builder(wc)
+                                        .setTitle(it.message)
+                                        .setMessage(it.stackTraceToString())
+                                        .show()
+                                }
+                            }.onSuccess {
+                                SyncUtils.runOnUiThread {
+                                    ChatActivityFacade.sendPttMessage(qqApp, session, save.absolutePath)
+                                    input.setText("")
+                                    Toasts.success(wc, "发送成功 (tryCount=$tryCount)")
+                                }
                             }
                         }
                 }
@@ -198,14 +187,4 @@ object SendTTSHook :
         trySend()
         return true
     }
-
-//    fun mp3ToSilk(mp3: String, silk: String): Boolean {
-//        File(mp3 + "m").delete()
-//        File(silk).delete()
-//        val bArr = ByteArray(1024 * 1024)
-//        val r = voiceRedPacketHelperImpl.invokeMethod("mixSong",
-//            args(bArr, mp3, silk, recorderParam),
-//            argTypes(ByteArray::class.java, String::class.java, String::class.java, Initiator._RecorderParam())) as Boolean
-//        return r
-//    }
 }
