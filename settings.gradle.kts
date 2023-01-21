ef909efc9da4d0ac68fc0f59c3bf9a96b229b067
keta1 <k@ketal.icu>
2023-01-21 13:46:34 +0800

    build: add gradle enterprise
    Signed-off-by: keta1 <k@ketal.icu>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,6 +22,19 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    `gradle-enterprise`
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlwaysIf(System.getenv("GITHUB_ACTIONS") == "true")
+        publishOnFailure()
+    }
+}
+
 rootProject.name = "QAuxiliary"
 include(
     ":app",
