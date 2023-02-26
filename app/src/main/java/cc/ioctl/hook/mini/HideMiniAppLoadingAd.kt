8a988b0d6f75f9e77a783c9fcef82285c6df1da7
ACh Sulfate <dex@libart.so>
2023-02-26 15:22:31 +0800

    feat: add HideMiniAppLoadingAd
    Signed-off-by: ACh Sulfate <dex@libart.so>

---
+++ app/src/main/java/cc/ioctl/hook/mini/HideMiniAppLoadingAd.kt
@@ -0,0 +1,60 @@
+/*
+ * QAuxiliary - An Xposed module for QQ/TIM
+ * Copyright (C) 2019-2023 QAuxiliary developers
+ * https://github.com/cinit/QAuxiliary
+ *
+ * This software is non-free but opensource software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version and our eula as published
+ * by QAuxiliary contributors.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * and eula along with this software.  If not, see
+ * <https://www.gnu.org/licenses/>
+ * <https://github.com/cinit/QAuxiliary/blob/master/LICENSE.md>.
+ */
+
+package cc.ioctl.hook.mini
+
+import cc.ioctl.util.hookBeforeIfEnabled
+import io.github.qauxv.base.annotation.FunctionHookEntry
+import io.github.qauxv.base.annotation.UiItemAgentEntry
+import io.github.qauxv.dsl.FunctionEntryRouter
+import io.github.qauxv.hook.CommonSwitchFunctionHook
+import io.github.qauxv.util.Initiator
+import io.github.qauxv.util.QQVersion
+import io.github.qauxv.util.SyncUtils
+import io.github.qauxv.util.TIMVersion
+import io.github.qauxv.util.requireMinQQVersion
+import io.github.qauxv.util.requireMinTimVersion
+
+@FunctionHookEntry
+@UiItemAgentEntry
+object HideMiniAppLoadingAd : CommonSwitchFunctionHook(
+    targetProc = SyncUtils.PROC_MAIN or SyncUtils.PROC_MINI
+) {
+
+    override val name = "隐藏小程序开屏广告"
+    override val description = "未经测试，如果小程序白屏30s以上或闪退请关闭此功能"
+
+    override val uiItemLocation = FunctionEntryRouter.Locations.Simplify.UI_MISC
+
+    override val isAvailable: Boolean
+        get() = requireMinQQVersion(QQVersion.QQ_8_3_9) || requireMinTimVersion(TIMVersion.TIM_3_5_0)
+
+    override fun initOnce(): Boolean {
+        val kMiniLoadingAdManager = Initiator.loadClass("com.tencent.qqmini.sdk.manager.MiniLoadingAdManager")
+        val method = kMiniLoadingAdManager.declaredMethods.single { it.name == "updateLoadingAdLayoutAndShow" }
+        hookBeforeIfEnabled(method) { param ->
+            param.result = null
+        }
+        return true
+    }
+
+}
