a859351c55cc764130e0a911c629a48603797f44
1001ace1001 <100484574+1001ace1001@users.noreply.github.com>
2023-04-06 22:50:35 +0800

    Update MultiActionHook.kt (#387)
    Update MultiActionHook.kt
    
    批量撤回的延迟时间减少到250ms

--- app/src/main/java/me/ketal/hook/MultiActionHook.kt
+++ app/src/main/java/me/ketal/hook/MultiActionHook.kt
@@ -90,7 +90,7 @@ object MultiActionHook : CommonSwitchFunctionHook(
                 if (list.isNotEmpty()) {
                     for (msg in list) {
                         QQMessageFacade.revokeMessage(msg)
-                        sleep(500)
+                        sleep(250)
                     }
                 }
                 SyncUtils.runOnUiThread {
