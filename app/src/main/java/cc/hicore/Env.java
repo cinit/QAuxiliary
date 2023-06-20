ea52f217a7fd33e72ba116855c482fe8a647ff1c
klxiaoniu <klxiaoniu@proton.me>
2023-06-20 12:46:31 +0800

    fix: Env app_save_path

--- app/src/main/java/cc/hicore/Env.java
+++ app/src/main/java/cc/hicore/Env.java
@@ -23,6 +23,6 @@
 package cc.hicore;
 
 public class Env {
-    public static String app_save_path = "/sdcard/Android/data/com.tencent.mobileqq/files/.tool";
+    public static String app_save_path = "/sdcard/Android/data/com.tencent.mobileqq/files/.tool/";
     public static Object AIOParam;
 }
