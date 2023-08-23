59f2b2ac1753ee04af8f8d6b71876daff3cd166b
‭huajijam <strhuaji@gmail.com>
2023-08-23 15:35:46 +0800

    fix: better desc when TroopUin not available

--- app/src/main/java/cc/ioctl/hook/msg/MultiForwardAvatarHook.kt
+++ app/src/main/java/cc/ioctl/hook/msg/MultiForwardAvatarHook.kt
@@ -287,7 +287,7 @@ object MultiForwardAvatarHook : CommonSwitchFunctionHook(arrayOf(CAIOUtils, Mult
         ll.setPadding(p, p / 3, p, p / 3)
         if (troopUin != null) {
             // troop
-            HostStyledViewBuilder.newDialogClickableItemClickToCopy(ctx, "群号", if (isTroopUinAvailable) troopUin.toString() else "群号已被腾讯服务器端屏蔽", ll, true) {
+            HostStyledViewBuilder.newDialogClickableItemClickToCopy(ctx, "群号", if (isTroopUinAvailable) troopUin.toString() else "已被服务器端屏蔽", ll, true) {
                 if (isTroopUinAvailable) {
                     OpenProfileCard.openTroopProfileActivity(ctx, troopUin.toString())
                 }
