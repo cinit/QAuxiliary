5e971180ae8b085e2e58e8dcb3ccf9924b938940
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-10-17 17:32:52 +0800

    feat(WIP): add Pride EasterEggs
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- app/src/main/java/me/singleneuron/util/EasterEggsForTheNonexistent.kt
+++ app/src/main/java/me/singleneuron/util/EasterEggsForTheNonexistent.kt
@@ -45,13 +45,20 @@ fun processSearchEasterEgg(text: String, context: Context) {
 private val easterEggsMap by lazy {
     hashMapOf(
         arrayOf("\u26A7\uFE0F", "\uD83C\uDF65", "mtf", "mtx", "ftm", "ftx", "transgender") to forSuBanXia,
-        arrayOf("喵") to ("喵喵" to "喵喵喵")
-    )
+        arrayOf("喵") to ("喵喵" to "喵喵喵"),
+        arrayOf("lgbt", "lgbtq", "lgbtqia", "lgbtqia+", "lesbian", "gay", "bisexual", "queer") to forPride
 }
 
 val forSuBanXia: Pair<String, String> = (String(Base64.decode("Rm9yIHVzIA==", Base64.DEFAULT)) + "\uD83C\uDFF3\uFE0F\u200D\u26A7\uFE0F" to String(
     Base64.decode(
         "5oS/5q+P5LiA5Liq5Lq66YO96IO96Ieq55Sx55qE55Sf5rS75Zyo6Ziz5YWJ5LiL77yM5oS/5oiR55qE6byT5Yqx5LiO5YuH5rCU6ZqPUUF1eGlsaWFyeeS8tOS9oOi6q+aXgeOAggoKCQkJCeKAlOKAlENyeW9saXRpYSwgYW4gZXhvcmRpbmFyeSBkZXZlbG9wZXIsIGFuIG9yZGluYXJ5IE10Rg==",
+        Base64.CRLF
+    )
+))
+
+val forPride: Pair<String, String> = (String(Base64.decode("Rm9yIFByaWRlIA==", Base64.DEFAULT)) + "\uF3F3\uFE0F\u200D\uF308" to String(
+    Base64.decode(
+        "TG92ZSBpcyBsb3ZlLiBGcm9tIGxvdmVseSBOZXh0QWxvbmUsIHdpdGggbG92ZS4=",
         Base64.CRLF
     )
 ))
