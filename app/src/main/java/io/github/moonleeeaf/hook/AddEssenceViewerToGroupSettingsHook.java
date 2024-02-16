4353a66a53b749f1e8f5ca7e4ed3a7c7b81c48ee
MoonLeeeaf <150461955+MoonLeeeaf@users.noreply.github.com>
2024-02-16 17:25:40 +0800

    add file header

--- app/src/main/java/io/github/moonleeeaf/hook/AddEssenceViewerToGroupSettingsHook.java
+++ app/src/main/java/io/github/moonleeeaf/hook/AddEssenceViewerToGroupSettingsHook.java
@@ -1,5 +1,27 @@
 package io.github.moonleeeaf.hook;
 
+/*
+ * QAuxiliary - An Xposed module for QQ/TIM
+ * Copyright (C) 2019-2022 qwq233@qwq2333.top
+ * https://github.com/cinit/QAuxiliary
+ *
+ * This software is non-free but opensource software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version and our eula as published
+ * by QAuxiliary contributors.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * and eula along with this software.  If not, see
+ * <https://www.gnu.org/licenses/>
+ * <https://github.com/cinit/QAuxiliary/blob/master/LICENSE.md>.
+ */
+
 import android.content.Intent;
 import android.view.View;
 import android.widget.Toast;
