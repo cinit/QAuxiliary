ec154ffd63c0cf9fd84931f01c0af092c7c280ad
fusumayuki <151672582+fusumayuki@users.noreply.github.com>
2024-04-08 11:16:21 +0800

    Change the supported versions of new ChatItemShowQQUin to QQ 8.9.93.13315+

--- app/src/main/java/me/ketal/hook/ChatItemShowQQUin.kt
+++ app/src/main/java/me/ketal/hook/ChatItemShowQQUin.kt
@@ -106,8 +106,6 @@ object ChatItemShowQQUin : CommonConfigFunctionHook(), OnBubbleBuilder {
 
     // X2J_APT <- ???Binding(com/tx/x2j/AioSenderBubbleTemplateBinding) <- AIOSenderBubbleTemplate
     private val NAME_TAIL_LAYOUT = when {
-        requireMinQQVersion(QQVersion.QQ_9_0_8) -> "srn"
-        requireMinQQVersion(QQVersion.QQ_8_9_93_BETA_13315) -> "so5"
         requireMinQQVersion(QQVersion.QQ_8_9_90) -> "smi"
         requireMinQQVersion(QQVersion.QQ_8_9_88) -> "slx"
         requireMinQQVersion(QQVersion.QQ_8_9_85) -> "sih"
@@ -357,7 +355,7 @@ object ChatItemShowQQUin : CommonConfigFunctionHook(), OnBubbleBuilder {
         val isFlashPicTagNeedShow = FlashPicHook.INSTANCE.isInitializationSuccessful && isFlashPicNt(chatMessage)
         if (!isEnabled && !isFlashPicTagNeedShow) return
 
-        if (requireMinQQVersion(QQVersion.QQ_9_0_15)) {
+        if (requireMinQQVersion(QQVersion.QQ_8_9_93_BETA_13315)) {
             if (!rootView.children.map { it.id }.contains(ID_ADD_LAYOUT)) {
                 val layout = LinearLayout(rootView.context).apply {
                     layoutParams = ConstraintLayout.LayoutParams(
