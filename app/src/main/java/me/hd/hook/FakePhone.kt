ee7f57a05edee92ce8fbc3114797c2c63e3e4b3b
hd <1839732296@qq.com>
2024-05-16 01:22:14 +0800

    refactor: FakePhone

--- app/src/main/java/me/hd/hook/FakePhone.kt
+++ app/src/main/java/me/hd/hook/FakePhone.kt
@@ -22,24 +22,75 @@
 
 package me.hd.hook
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
-import cc.ioctl.util.hookBeforeIfEnabled
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.customview.customView
+import com.afollestad.materialdialogs.customview.getCustomView
+import com.afollestad.materialdialogs.input.getInputField
+import com.afollestad.materialdialogs.input.input
+import com.github.kyuubiran.ezxhelper.utils.hookBefore
 import io.github.qauxv.base.annotation.FunctionHookEntry
 import io.github.qauxv.base.annotation.UiItemAgentEntry
+import io.github.qauxv.core.HookInstaller
 import io.github.qauxv.dsl.FunctionEntryRouter
-import io.github.qauxv.hook.CommonSwitchFunctionHook
+import io.github.qauxv.dsl.uiClickableItem
+import io.github.qauxv.hook.BaseFunctionHook
 import io.github.qauxv.util.Initiator
 import io.github.qauxv.util.QQVersion
 import io.github.qauxv.util.requireMinQQVersion
+import me.ketal.data.ConfigData
+import me.ketal.ui.view.ConfigView
+import me.ketal.util.ignoreResult
 
 @FunctionHookEntry
 @UiItemAgentEntry
-object FakePhone : CommonSwitchFunctionHook() {
+object FakePhone : BaseFunctionHook("hd_FakePhone") {
 
-    override val name = "伪装手机号码"
-    override val description = "伪装设置页手机号码显示内容"
-    override val uiItemLocation = FunctionEntryRouter.Locations.Entertainment.ENTERTAIN_CATEGORY
+    private val phoneKey = ConfigData<String>("hd_FakePhone_phone")
+    private var phone: String
+        get() = phoneKey.getOrDefault("100******00")
+        set(value) {
+            phoneKey.value = value
+        }
+
+    override val uiItemAgent by lazy {
+        uiClickableItem {
+            title = "伪装手机号码"
+            summary = "伪装设置页手机号码显示内容"
+            onClickListener = { _, activity, _ ->
+                showDialog(activity)
+            }
+        }
+    }
+    override val uiItemLocation = FunctionEntryRouter.Locations.Simplify.UI_MISC
     override val isAvailable = requireMinQQVersion(QQVersion.QQ_8_9_88)
+
+    @SuppressLint("RestrictedApi")
+    private fun showDialog(activity: Context) {
+        val configView = ConfigView(activity)
+        val dialog = MaterialDialog(activity).show {
+            title(text = "伪装手机号码")
+            input(
+                hint = "请输入手机号码...",
+                prefill = phone
+            ).ignoreResult()
+            positiveButton(text = "保存") {
+                isEnabled = configView.isChecked
+                if (isEnabled) {
+                    phone = getInputField().text.toString()
+                    if (!isInitialized) HookInstaller.initializeHookForeground(context, this@FakePhone)
+                }
+            }
+            negativeButton(text = "取消")
+        }
+        configView.setText("伪装手机号码")
+        configView.view = dialog.getCustomView()
+        configView.isChecked = isEnabled
+        dialog.view.contentLayout.customView = null
+        dialog.customView(view = configView)
+    }
 
     override fun initOnce(): Boolean {
         /**
@@ -58,11 +109,13 @@ object FakePhone : CommonSwitchFunctionHook() {
                 else -> return false
             }
         )
-        val onUpdateMethod = onUpdateClass.getDeclaredMethod("onUpdate", Int::class.java, Boolean::class.java, Object::class.java)
-        hookBeforeIfEnabled(onUpdateMethod) { param ->
+        val onUpdateMethod = onUpdateClass.getDeclaredMethod(
+            "onUpdate", Int::class.java, Boolean::class.java, Object::class.java
+        )
+        onUpdateMethod.hookBefore { param ->
             if (param.args[0] == 5) {
                 val bundle = param.args[2] as Bundle
-                bundle.putString("phone", "100******00")
+                bundle.putString("phone", phone)
                 param.args[2] = bundle
             }
         }
