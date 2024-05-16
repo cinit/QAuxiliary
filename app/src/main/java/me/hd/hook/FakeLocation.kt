7512da5f8208c2df804a5f090bbd6ab9bdb413b9
hd <1839732296@qq.com>
2024-05-16 18:35:53 +0800

    refactor FakeLocation

--- app/src/main/java/me/hd/hook/FakeLocation.kt
+++ app/src/main/java/me/hd/hook/FakeLocation.kt
@@ -22,23 +22,102 @@
 
 package me.hd.hook
 
+import android.annotation.SuppressLint
+import android.content.Context
 import cc.ioctl.util.hookBeforeIfEnabled
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.WhichButton
+import com.afollestad.materialdialogs.actions.setActionButtonEnabled
+import com.afollestad.materialdialogs.customview.customView
+import com.afollestad.materialdialogs.customview.getCustomView
+import com.afollestad.materialdialogs.input.getInputField
+import com.afollestad.materialdialogs.input.input
 import io.github.qauxv.base.annotation.FunctionHookEntry
 import io.github.qauxv.base.annotation.UiItemAgentEntry
+import io.github.qauxv.core.HookInstaller
 import io.github.qauxv.dsl.FunctionEntryRouter
-import io.github.qauxv.hook.CommonSwitchFunctionHook
+import io.github.qauxv.dsl.uiClickableItem
+import io.github.qauxv.hook.BaseFunctionHook
 import io.github.qauxv.util.Initiator
 import io.github.qauxv.util.QQVersion
 import io.github.qauxv.util.requireMinQQVersion
+import me.ketal.data.ConfigData
+import me.ketal.ui.view.ConfigView
+import me.ketal.util.ignoreResult
 
 @FunctionHookEntry
 @UiItemAgentEntry
-object FakeLocation : CommonSwitchFunctionHook() {
+object FakeLocation : BaseFunctionHook("hd_FakeLocation") {
 
-    override val name = "虚拟共享位置"
-    override val description = "虚拟群聊内共享位置定位内容"
+    private val locationKey = ConfigData<String>("hd_FakeLocation_phone")
+    private var location: String
+        get() = locationKey.getOrDefault("116.39773,39.90307")//(经度,纬度)
+        set(value) {
+            locationKey.value = value
+        }
+
+    override val uiItemAgent by lazy {
+        uiClickableItem {
+            title = "虚拟共享位置"
+            summary = "虚拟群聊内共享位置定位内容"
+            onClickListener = { _, activity, _ ->
+                showDialog(activity)
+            }
+        }
+    }
     override val uiItemLocation = FunctionEntryRouter.Locations.Entertainment.ENTERTAIN_CATEGORY
     override val isAvailable = requireMinQQVersion(QQVersion.QQ_8_9_88)
+
+    @SuppressLint("RestrictedApi")
+    private fun showDialog(activity: Context) {
+        val configView = ConfigView(activity)
+        val dialog = MaterialDialog(activity).show {
+            title(text = "虚拟共享位置")
+            input(
+                hint = "经度,纬度",
+                prefill = location,
+                waitForPositiveButton = false
+            ) { dialog, text ->
+                val inputField = dialog.getInputField()
+                dialog.setActionButtonEnabled(
+                    WhichButton.POSITIVE,
+                    try {
+                        val locationArray = text.toString().split(",")
+                        if (locationArray.size == 2) {
+                            val longitude = locationArray[0].toDouble()
+                            val latitude = locationArray[1].toDouble()
+                            if (longitude in -180.0..180.0 && latitude in -90.0..90.0) {
+                                inputField.error = null
+                                true
+                            } else {
+                                inputField.error = "无效数值，经度范围为-180~180，纬度范围为-90~90"
+                                false
+                            }
+                        } else {
+                            inputField.error = "无效格式，格式应为经度,纬度"
+                            false
+                        }
+                    } catch (e: NumberFormatException) {
+                        inputField.error = "请输入有效的数据"
+                        false
+                    }
+                )
+            }.ignoreResult()
+            positiveButton(text = "保存") {
+                isEnabled = configView.isChecked
+                if (isEnabled) {
+                    location = getInputField().text.toString()
+                    if (!isInitialized) HookInstaller.initializeHookForeground(context, this@FakeLocation)
+                }
+            }
+            negativeButton(text = "取消")
+        }
+        configView.setText("虚拟共享位置")
+        configView.view = dialog.getCustomView()
+        configView.isChecked = isEnabled
+        dialog.view.contentLayout.customView = null
+        dialog.customView(view = configView)
+    }
 
     override fun initOnce(): Boolean {
         val locationManagerClass = Initiator.loadClass("com.tencent.mobileqq.location.net.LocationShareLocationManager\$a")
@@ -46,11 +125,11 @@ object FakeLocation : CommonSwitchFunctionHook() {
         val onChangedMethod = locationManagerClass.getDeclaredMethod("onLocationChanged", locationInterfaceClass, Int::class.java, String::class.java)
         hookBeforeIfEnabled(onChangedMethod) { param ->
             val locationClass = param.args[0]::class.java
-            hookBeforeIfEnabled(locationClass.getDeclaredMethod("getLatitude")) { paramLatitude ->
-                paramLatitude.result = 39.90307
-            }
             hookBeforeIfEnabled(locationClass.getDeclaredMethod("getLongitude")) { paramLongitude ->
-                paramLongitude.result = 116.39773
+                paramLongitude.result = location.split(",")[0].toDouble()
+            }
+            hookBeforeIfEnabled(locationClass.getDeclaredMethod("getLatitude")) { paramLatitude ->
+                paramLatitude.result = location.split(",")[1].toDouble()
             }
         }
         return true
