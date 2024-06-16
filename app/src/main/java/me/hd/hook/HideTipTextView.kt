58070d47bcf7f86d367de108c9bd8bb286692bcf
hd <1839732296@qq.com>
2024-06-16 10:10:37 +0800

    refactor: HideClockInTip to HideTipTextView

--- app/src/main/java/me/hd/hook/HideTipTextView.kt
+++ app/src/main/java/me/hd/hook/HideTipTextView.kt
@@ -25,40 +25,52 @@ package me.hd.hook
 import android.view.ViewGroup
 import android.widget.TextView
 import cc.ioctl.util.hookAfterIfEnabled
+import de.robv.android.xposed.XposedHelpers
 import io.github.qauxv.base.annotation.FunctionHookEntry
 import io.github.qauxv.base.annotation.UiItemAgentEntry
 import io.github.qauxv.dsl.FunctionEntryRouter
-import io.github.qauxv.hook.CommonSwitchFunctionHook
 import io.github.qauxv.util.Initiator
 import io.github.qauxv.util.QQVersion
 import io.github.qauxv.util.requireMinQQVersion
+import xyz.nextalone.base.MultiItemDelayableHook
 import xyz.nextalone.util.isPrivate
+import xyz.nextalone.util.isPublic
 
 @FunctionHookEntry
 @UiItemAgentEntry
-object HideClockInTip : CommonSwitchFunctionHook() {
+object HideTipTextView : MultiItemDelayableHook("hd_HideTipTextView") {
 
-    override val name = "隐藏打卡消息"
-    override val description = "隐藏群聊打卡消息, 可能导致聊天界面滑动掉帧"
-    override val uiItemLocation = FunctionEntryRouter.Locations.Simplify.CHAT_GROUP_OTHER
+    override val preferenceTitle = "隐藏提示文本消息"
+    override val description = "当提示文本消息过多, 可能导致聊天界面滑动掉帧"
+    override val dialogDesc = "隐藏"
+    override val allItems = setOf(
+        "我也要打卡",
+        "加入了群聊",
+    )
+    override val uiItemLocation = FunctionEntryRouter.Locations.Simplify.UI_CHAT_MSG
     override val isAvailable = requireMinQQVersion(QQVersion.QQ_8_9_88)
 
     override fun initOnce(): Boolean {
         val tipsClass = Initiator.loadClass("com.tencent.mobileqq.aio.msglist.holder.component.graptips.common.CommonGrayTipsComponent")
-        val textViewClass = if (requireMinQQVersion(QQVersion.QQ_9_0_8)) {
-            Initiator.loadClass("com.tencent.qqnt.aio.widget.AIOMsgTextView")
-        } else {
-            Initiator.loadClass("com.tencent.mobileqq.aio.msglist.holder.component.graptips.GrayTipsTextView")
+        val initMethod = tipsClass.declaredMethods.single { method ->
+            val params = method.parameterTypes
+            method.isPublic && method.returnType == Void.TYPE && params.size == 3 && params[0] == Int::class.java && params[2] == List::class.java
         }
         val getTextViewMethod = tipsClass.declaredMethods.single { method ->
-            method.isPrivate && method.returnType == textViewClass
+            method.isPrivate && method.returnType == if (requireMinQQVersion(QQVersion.QQ_9_0_8)) {
+                Initiator.loadClass("com.tencent.qqnt.aio.widget.AIOMsgTextView")
+            } else {
+                Initiator.loadClass("com.tencent.mobileqq.aio.msglist.holder.component.graptips.GrayTipsTextView")
+            }
         }
-        hookAfterIfEnabled(getTextViewMethod) { param ->
-            val textView = param.result as TextView
-            val text = textView.text.toString()
-            if (text.endsWith("我也要打卡")) {
-                val parent = textView.parent.parent as ViewGroup
-                parent.layoutParams = ViewGroup.LayoutParams(0, 0)
+        hookAfterIfEnabled(initMethod) {
+            val textView = XposedHelpers.callMethod(it.thisObject, getTextViewMethod.name) as TextView
+            activeItems.forEach { item ->
+                if (textView.text.toString().contains(item)) {
+                    val parent = textView.parent.parent as ViewGroup
+                    parent.layoutParams = ViewGroup.LayoutParams(0, 0)
+                    return@forEach
+                }
             }
         }
         return true
