a74b674f8a37b7bc7502cc66deddb05a76e79a22
hd <1839732296@qq.com>
2024-07-02 16:39:45 +0800

    chore: update QQ9.0.71 version

--- app/src/main/java/io/github/qauxv/util/QQVersion.java
+++ app/src/main/java/io/github/qauxv/util/QQVersion.java
@@ -124,4 +124,5 @@ public class QQVersion {
     public static final long QQ_9_0_65 = 6566;
     public static final long QQ_9_0_68 = 6612;
     public static final long QQ_9_0_70 = 6676;
+    public static final long QQ_9_0_71 = 6702;
 }
