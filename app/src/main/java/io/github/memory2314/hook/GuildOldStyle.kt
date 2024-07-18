1a6ade733a08e26e7d32ce656ed684df1204743a
Memory <134070804+Memory2314@users.noreply.github.com>
2024-07-18 13:59:54 +0800

    feat: GuildOldStyle

---
+++ app/src/main/java/io/github/memory2314/hook/GuildOldStyle.kt
@@ -0,0 +1,40 @@
+/*
+ * QAuxiliary - An Xposed module for QQ/TIM
+ * Copyright (C) 2019-2022 qwq233@qwq2333.top
+ * https://github.com/cinit/QAuxiliary
+ *
+ * This software is non-free but opensource software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version and our eula as published
+ * by QAuxiliary contributors.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * and eula along with this software.  If not, see
+ * <https://www.gnu.org/licenses/>
+ * <https://github.com/cinit/QAuxiliary/blob/master/LICENSE.md>.
+ */
+package io.github.memory2314.hook
+
+import io.github.qauxv.base.annotation.FunctionHookEntryimport io.github.qauxv.base.annotation.UiItemAgentEntry
+import io.github.qauxv.dsl.FunctionEntryRouterimport io.github.qauxv.hook.CommonSwitchFunctionHook
+import io.github.qauxv.util.QQVersionimport io.github.qauxv.util.requireMinQQVersion
+import xyz.nextalone.util.methodimport xyz.nextalone.util.replace
+import xyz.nextalone.util.throwOrTrue
+
+@FunctionHookEntry
+@UiItemAgentEntry
+object GuildOldStyle : CommonSwitchFunctionHook() {
+    override val name = "频道旧版样式"
+    override val description = "当前仅适配QQ9.0.0"    override val uiItemLocation = FunctionEntryRouter.Locations.Auxiliary.GUILD_CATEGORY
+    override val isAvailable = requireMinQQVersion(QQVersion.QQ_9_0_0)
+    
+    override fun initOnce() = throwOrTrue {
+        "Lcom/tencent/mobileqq/guild/discover/GuildDiscoverABTestHelper;->w()I".method.replace(this, 1)
+    }
+}
