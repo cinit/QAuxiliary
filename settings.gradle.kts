fb8141af679196d0fb788d1e016c5dadfbb4406f
ACh Sulfate <dex@libart.so>
2024-11-23 22:12:22 +0800

    chore: deps: add workaround for jitpack failures

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -34,6 +34,16 @@ dependencyResolutionManagement {
             }
         }
         mavenCentral()
+        maven("https://maven.tmpfs.dev/repository/maven-public/") {
+            // I have no idea why sometimes jitpack.io is not working for
+            // "com.github.plattysoft:Leonids:1746429"
+            // So I added this repo as a backup.
+            // I have encountered this wired issue twice in 2024.
+            // The jitpack.io says "Not found" or "File not found. Build ok".
+            content {
+                includeGroup("com.github.plattysoft")
+            }
+        }
     }
 }
 
