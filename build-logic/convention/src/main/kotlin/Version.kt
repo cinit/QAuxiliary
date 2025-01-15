651aa68ccd5d4e7c59f57c5237e8a9d96e2d84e7
ACh Sulfate <40174090+cinit@users.noreply.github.com>
2025-01-15 21:45:47 +0800

    bump version 1.5.6

--- build-logic/convention/src/main/kotlin/Version.kt
+++ build-logic/convention/src/main/kotlin/Version.kt
@@ -10,7 +10,7 @@ object Version {
     val buildToolsVersion = findBuildToolsVersion()
     const val minSdk = 24
     const val targetSdk = 35
-    const val versionName = "1.5.5"
+    const val versionName = "1.5.6"
 
     private const val defaultNdkVersion = "27.0.12077973"
 
