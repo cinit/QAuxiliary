671c33cd4158d21e0039a62274da921caf5ddd95
EX3124 <70867298+EX3124@users.noreply.github.com>
2025-03-21 19:25:49 +0800

    fix: HideChatPanelBtn on QQ 8.2.11

--- app/src/main/java/me/hd/hook/HideChatPanelBtn.kt
+++ app/src/main/java/me/hd/hook/HideChatPanelBtn.kt
@@ -32,6 +32,9 @@ import io.github.qauxv.util.Initiator
 import io.github.qauxv.util.QQVersion
 import io.github.qauxv.util.requireMinQQVersion
 import xyz.nextalone.base.MultiItemDelayableHook
+import io.github.qauxv.util.PlayQQVersion
+import io.github.qauxv.util.requireRangePlayQQVersion
+import cc.ioctl.util.hookBeforeIfEnabled
 
 @FunctionHookEntry
 @UiItemAgentEntry
@@ -39,21 +42,42 @@ object HideChatPanelBtn : MultiItemDelayableHook(
     keyName = "hd_HideChatPanelBtn"
 ) {
     override val preferenceTitle = "屏蔽聊天面板按钮"
-    override val allItems = setOf("语音", "拍照", "红包", "表情", "更多功能")
+    override val allItems = if (requireRangePlayQQVersion(PlayQQVersion.PlayQQ_8_2_11, PlayQQVersion.PlayQQ_8_2_11)) setOf("语音", "图片", "拍照", "红包", "表情", "更多功能", "文件(我的电脑)", "热图(临时会话)", "电脑(我的电脑)", "拍照(临时会话)", "定位(临时会话)") else setOf("语音", "拍照", "红包", "表情", "更多功能")
     override val defaultItems = setOf<String>()
     override val uiItemLocation = FunctionEntryRouter.Locations.Simplify.CHAT_OTHER
-    override val isAvailable = requireMinQQVersion(QQVersion.QQ_8_9_88)
+    override val isAvailable = requireMinQQVersion(QQVersion.QQ_8_9_88) || requireRangePlayQQVersion(PlayQQVersion.PlayQQ_8_2_11, PlayQQVersion.PlayQQ_8_2_11)
 
     override fun initOnce(): Boolean {
-        val panelIconClass = Initiator.loadClass("com.tencent.qqnt.aio.shortcutbar.PanelIconLinearLayout")
-        val iconItemMethod = panelIconClass.declaredMethods.single { method ->
-            method.returnType == ImageView::class.java
-        }
-        hookAfterIfEnabled(iconItemMethod) { param ->
-            val imageView = param.result as ImageView
-            val contentDesc = imageView.contentDescription
-            if (activeItems.contains(contentDesc)) {
-                imageView.visibility = View.GONE
+        if (requireRangePlayQQVersion(PlayQQVersion.PlayQQ_8_2_11, PlayQQVersion.PlayQQ_8_2_11)) {
+            hookBeforeIfEnabled(Initiator.loadClass("ayil").getDeclaredMethod("b", android.content.Context::class.java, View::class.java)) { param ->
+                val bar = param.args[1] as View
+                val allItemsMap = mapOf(
+                    "语音" to bar.findViewById<View>(0x7f0a2b72),
+                    "图片" to bar.findViewById<View>(0x7f0a2b68),
+                    "拍照" to bar.findViewById<View>(0x7f0a2b75),
+                    "红包" to bar.findViewById<View>(0x7f0a2b60),
+                    "表情" to bar.findViewById<View>(0x7f0a2b56),
+                    "更多功能" to bar.findViewById<View>(0x7f0a2b6d),
+                    "文件(我的电脑)" to bar.findViewById<View>(0x7f0a2b5d),
+                    "热图(临时会话)" to bar.findViewById<View>(0x7f0a2b61),
+                    "电脑(我的电脑)" to bar.findViewById<View>(0x7f0a2b6b),
+                    "拍照(临时会话)" to bar.findViewById<View>(0x7f0a2b50),
+                    "定位(临时会话)" to bar.findViewById<View>(0x7f0a2b71)
+                )
+                for (item in activeItems)
+                    allItemsMap[item]?.visibility = View.GONE
+            }
+        } else {
+            val panelIconClass = Initiator.loadClass("com.tencent.qqnt.aio.shortcutbar.PanelIconLinearLayout")
+            val iconItemMethod = panelIconClass.declaredMethods.single { method ->
+                method.returnType == ImageView::class.java
+            }
+            hookAfterIfEnabled(iconItemMethod) { param ->
+                val imageView = param.result as ImageView
+                val contentDesc = imageView.contentDescription
+                if (activeItems.contains(contentDesc)) {
+                    imageView.visibility = View.GONE
+                }
             }
         }
         return true
