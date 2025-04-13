b860faf9b19e060c3c4267538eebbb43cb832e13
明熙 <158139013+hiatus169@users.noreply.github.com>
2025-04-13 11:36:18 +0800

    Create BlockLinkInfo.kt

---
+++ app/src/main/java/im/mingxi/BlockLinkInfo.kt
@@ -0,0 +1,30 @@
+package im.mingxi
+
+import io.github.qauxv.base.annotation.FunctionHookEntry
+import io.github.qauxv.base.annotation.UiItemAgentEntry
+import io.github.qauxv.dsl.FunctionEntryRouter
+import io.github.qauxv.hook.CommonSwitchFunctionHook
+import io.github.qauxv.util.Initiator
+import io.github.qauxv.util.xpcompat.XC_MethodHook
+import io.github.qauxv.util.xpcompat.XposedBridge
+
+@FunctionHookEntry
+@UiItemAgentEntry
+object RemoveShakeAdExampleHook : CommonSwitchFunctionHook() {
+
+    override val name = "屏蔽链接信息"
+
+    override val description = "去除链接下方的信息卡片，也可以用来防耗流量链接消息"
+
+    override val uiItemLocation: Array<String> = FunctionEntryRouter.Locations.Auxiliary.CHAT_CATEGORY
+
+    override fun initOnce(): Boolean {
+        val linkInfoClass = Initiator.loadClass("com.tencent.qqnt.kernel.nativeinterface.LinkInfo")
+        XposedBridge.hookAllConstructors(linkInfoClass, object: XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                param.result = null
+            }
+        })
+        return true
+    }
+}
