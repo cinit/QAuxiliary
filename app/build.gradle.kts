d4d15c7ea6bb8b0e5ff7ba39a7116f6009f993e6
ACh Sulfate <dex@libart.so>
2025-05-08 21:37:16 +0800

    build: move debug build options to local.properties

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,9 +50,12 @@ plugins {
 
 // ------ buildscript config ------
 
-val buildAllAbiForDebug = false
-val isNewXposedApiEnabled = true
-val isNativeFullDebugMode = false
+val buildAllAbiForDebug = Version.getLocalProperty(project, "qauxv.override.forceallabi")
+    ?.toBoolean() ?: false
+val isNewXposedApiEnabled = Version.getLocalProperty(project, "qauxv.override.newxposedapi")
+    ?.toBoolean() ?: true
+val isNativeFullDebugMode = Version.getLocalProperty(project, "qauxv.override.nativefulldebug")
+    ?.toBoolean() ?: false
 
 val currentBuildUuid = UUID.randomUUID().toString()
 println("Current build ID is $currentBuildUuid")
