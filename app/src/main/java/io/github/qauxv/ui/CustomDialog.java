d136d6df1b795c0835d321ba134cc70aaba54621
ACh Sulfate <dex@libart.so>
2025-08-03 18:46:20 +0800

    refactor: remove legacy QQ style dialog

--- app/src/main/java/io/github/qauxv/ui/CustomDialog.java
+++ app/src/main/java/io/github/qauxv/ui/CustomDialog.java
@@ -21,98 +21,22 @@
  */
 package io.github.qauxv.ui;
 
-import static io.github.qauxv.util.Initiator.load;
-
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.view.View;
-import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import cc.ioctl.util.Reflex;
-import io.github.qauxv.base.annotation.DexDeobfs;
-import io.github.qauxv.util.Log;
-import io.github.qauxv.util.dexkit.CDialogUtil;
-import io.github.qauxv.util.dexkit.DexKit;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 
 public class CustomDialog {
 
-    private static Class<?> clz_DialogUtil;
-    private static Class<?> clz_CustomDialog;
-    private static Method m_DialogUtil_a;
-    private Dialog mDialog = null;
     private androidx.appcompat.app.AlertDialog mFailsafeDialog = null;
     private androidx.appcompat.app.AlertDialog.Builder mBuilder = null;
-    private boolean failsafe = false;
 
-    @DexDeobfs(CDialogUtil.class)
     public static CustomDialog create(Context ctx) {
-        try {
-            if (clz_DialogUtil == null) {
-                clz_DialogUtil = DexKit.loadClassFromCache(CDialogUtil.INSTANCE);
-            }
-            if (clz_CustomDialog == null) {
-                clz_CustomDialog = load("com/tencent/mobileqq/utils/QQCustomDialog");
-                if (clz_CustomDialog == null) {
-                    Class clz_Lite = load("com/dataline/activities/LiteActivity");
-                    Field[] fs = clz_Lite.getDeclaredFields();
-                    for (Field f : fs) {
-                        if (Modifier.isPrivate(f.getModifiers()) && Dialog.class.isAssignableFrom(f.getType())) {
-                            clz_CustomDialog = f.getType();
-                            break;
-                        }
-                    }
-                }
-            }
-            if (m_DialogUtil_a == null) {
-                Method tmpa = null, tmpb = null;
-                for (Method m : clz_DialogUtil.getDeclaredMethods()) {
-                    if (m.getReturnType().equals(clz_CustomDialog) && (Modifier.isPublic(m.getModifiers()))) {
-                        Class<?>[] argt = m.getParameterTypes();
-                        if (argt.length != 2) {
-                            continue;
-                        }
-                        if (argt[0].equals(Context.class) && argt[1].equals(int.class)) {
-                            if (m.getName().equals("a")) {
-                                m_DialogUtil_a = m;
-                                break;
-                            } else {
-                                if (tmpa == null) {
-                                    tmpa = m;
-                                } else {
-                                    tmpb = m;
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-                if (m_DialogUtil_a == null && tmpa != null) {
-                    if (tmpb == null) {
-                        m_DialogUtil_a = tmpa;
-                    } else {
-                        m_DialogUtil_a = (Reflex.strcmp(tmpa.getName(), tmpb.getName()) > 0) ? tmpb : tmpa;
-                    }
-                }
-            }
-        } catch (Exception e) {
-            Log.e(e);
-        }
         CustomDialog ref = new CustomDialog();
-        try {
-            ref.mDialog = (Dialog) m_DialogUtil_a.invoke(null, ctx, 230);
-        } catch (Exception e) {
-            Log.e(e);
-        }
-        if (ref.mDialog == null) {
-            ref.failsafe = true;
-            ref.mBuilder = new androidx.appcompat.app.AlertDialog.Builder(
-                    CommonContextWrapper.createAppCompatContext(ctx));
-        }
+        // dark/light theme is already handled by CommonContextWrapper.createAppCompatContext(context)
+        ref.mBuilder = new androidx.appcompat.app.AlertDialog.Builder(CommonContextWrapper.createAppCompatContext(ctx));
         return ref;
     }
 
@@ -123,85 +47,46 @@ public class CustomDialog {
      */
     @Deprecated
     public static CustomDialog createFailsafe(Context context) {
-        CustomDialog ref = new CustomDialog();
-        // dark/light theme is already handled by CommonContextWrapper.createAppCompatContext(context)
-        Context ctx = CommonContextWrapper.createAppCompatContext(context);
-        ref.failsafe = true;
-        ref.mBuilder = new androidx.appcompat.app.AlertDialog.Builder(ctx);
-        return ref;
+        // deprecated.
+        return create(context);
     }
 
     public CustomDialog setCancelable(boolean flag) {
-        if (!failsafe) {
-            mDialog.setCancelable(flag);
-            if (flag) {
-                mDialog.setCanceledOnTouchOutside(true);
-            }
-        } else {
-            mBuilder.setCancelable(flag);
-        }
+        mBuilder.setCancelable(flag);
         return this;
     }
 
     public CustomDialog setTitle(String title) {
-        if (!failsafe) {
-            try {
-                Reflex.invokeVirtual(mDialog, "setTitle", title, String.class);
-            } catch (Exception e) {
-                Log.e(e);
-            }
+        if (mFailsafeDialog == null) {
+            mBuilder.setTitle(title);
         } else {
-            if (mFailsafeDialog == null) {
-                mBuilder.setTitle(title);
-            } else {
-                mFailsafeDialog.setTitle(title);
-            }
+            mFailsafeDialog.setTitle(title);
         }
         return this;
     }
 
     public CustomDialog setMessage(CharSequence msg) {
-        if (!failsafe) {
-            try {
-                Reflex.invokeVirtual(mDialog, "setMessage", msg, CharSequence.class);
-            } catch (Exception e) {
-                Log.e(e);
-            }
+        if (mFailsafeDialog == null) {
+            mBuilder.setMessage(msg);
         } else {
-            if (mFailsafeDialog == null) {
-                mBuilder.setMessage(msg);
-            } else {
-                mFailsafeDialog.setMessage(msg);
-            }
+            mFailsafeDialog.setMessage(msg);
         }
         return this;
     }
 
     public Context getContext() {
-        if (!failsafe) {
-            return mDialog.getContext();
+        if (mFailsafeDialog == null) {
+            return mBuilder.getContext();
         } else {
-            if (mFailsafeDialog == null) {
-                return mBuilder.getContext();
-            } else {
-                return mFailsafeDialog.getContext();
-            }
+            return mFailsafeDialog.getContext();
         }
     }
 
     public CustomDialog setView(View v) {
-        if (!failsafe) {
-            try {
-                Reflex.invokeVirtual(mDialog, "setView", v, View.class);
-            } catch (Exception e) {
-                Log.e(e);
-            }
+        if (mFailsafeDialog == null) {
+            mBuilder.setView(v);
         } else {
-            if (mFailsafeDialog == null) {
-                mBuilder.setView(v);
-            } else {
-                mFailsafeDialog.setView(v);
-            }
+            mFailsafeDialog.setView(v);
         }
         return this;
     }
@@ -209,22 +94,15 @@ public class CustomDialog {
     @NonNull
     public CustomDialog setPositiveButton(int text, @Nullable DialogInterface.OnClickListener listener) {
         Context ctx;
-        if (failsafe) {
-            ctx = mBuilder.getContext();
-        } else {
-            ctx = mDialog.getContext();
-        }
-        return setPositiveButton(ctx.getString(text), listener);
+        ctx = mBuilder.getContext();
+        setPositiveButton(ctx.getString(text), listener);
+        return this;
     }
 
     @NonNull
     public CustomDialog setNegativeButton(int text, @Nullable DialogInterface.OnClickListener listener) {
         Context ctx;
-        if (failsafe) {
-            ctx = mBuilder.getContext();
-        } else {
-            ctx = mDialog.getContext();
-        }
+        ctx = mBuilder.getContext();
         return setNegativeButton(ctx.getString(text), listener);
     }
 
@@ -236,109 +114,53 @@ public class CustomDialog {
 
     @NonNull
     public CustomDialog setPositiveButton(@NonNull String text, @Nullable DialogInterface.OnClickListener listener) {
-        if (!failsafe) {
-            if (text != null && listener == null) {
-                listener = new DummyCallback();
-            }
-            try {
-                Reflex.invokeVirtual(mDialog, "setPositiveButton", text, listener, String.class, DialogInterface.OnClickListener.class);
-            } catch (Exception e) {
-                Log.e(e);
-            }
-        } else {
-            mBuilder.setPositiveButton(text, listener);
-        }
+        mBuilder.setPositiveButton(text, listener);
         return this;
     }
 
     @NonNull
     public CustomDialog setNeutralButton(@NonNull String text, @Nullable DialogInterface.OnClickListener listener) {
-        if (!failsafe) {
-            //They don't have a neutral button, sigh...
-        } else {
-            mBuilder.setNeutralButton(text, listener);
-        }
+        mBuilder.setNeutralButton(text, listener);
         return this;
     }
 
     @NonNull
     public CustomDialog setNeutralButton(int text, @Nullable DialogInterface.OnClickListener listener) {
-        if (!failsafe) {
-            //They don't have a neutral button, sigh...
-        } else {
-            mBuilder.setNeutralButton(text, listener);
-        }
+        mBuilder.setNeutralButton(text, listener);
         return this;
     }
 
     public CustomDialog setNegativeButton(String text, DialogInterface.OnClickListener listener) {
-        if (!failsafe) {
-            if (text != null && listener == null) {
-                listener = new DummyCallback();
-            }
-            try {
-                Reflex.invokeVirtual(mDialog, "setNegativeButton", text, listener, String.class,
-                    DialogInterface.OnClickListener.class);
-            } catch (Exception e) {
-                Log.e(e);
-            }
-        } else {
-            mBuilder.setNegativeButton(text, listener);
-        }
+        mBuilder.setNegativeButton(text, listener);
         return this;
     }
 
     public Dialog create() {
-        if (!failsafe) {
-            return mDialog;
-        } else {
-            if (mFailsafeDialog == null) {
-                mFailsafeDialog = mBuilder.create();
-            }
-            return mFailsafeDialog;
+        if (mFailsafeDialog == null) {
+            mFailsafeDialog = mBuilder.create();
         }
+        return mFailsafeDialog;
     }
 
     public Dialog show() {
-        if (!failsafe) {
-            mDialog.show();
-            return mDialog;
-        } else {
-            if (mFailsafeDialog == null) {
-                mFailsafeDialog = mBuilder.create();
-            }
-            mFailsafeDialog.show();
-            return mFailsafeDialog;
+        if (mFailsafeDialog == null) {
+            mFailsafeDialog = mBuilder.create();
         }
+        mFailsafeDialog.show();
+        return mFailsafeDialog;
     }
 
     public void dismiss() {
-        if (!failsafe) {
-            mDialog.dismiss();
-        } else {
-            if (mFailsafeDialog != null) {
-                mFailsafeDialog.dismiss();
-            }
+        if (mFailsafeDialog != null) {
+            mFailsafeDialog.dismiss();
         }
     }
 
     public boolean isShowing() {
-        if (mDialog != null) {
-            return mDialog.isShowing();
-        }
         if (mFailsafeDialog != null) {
             return mFailsafeDialog.isShowing();
         }
         return false;
-    }
-
-    @Nullable
-    public TextView getMessageTextView() {
-        if (!failsafe) {
-            return (TextView) Reflex.getInstanceObjectOrNull(mDialog, "text");
-        } else {
-            return null;
-        }
     }
 
     public static class DummyCallback implements DialogInterface.OnClickListener {
