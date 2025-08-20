278f4888f3efbf9ed52fffa3f5ee5a5a211c7d48
ACh Sulfate <dex@libart.so>
2025-08-20 20:28:48 +0800

    chore: update target sdk to 36

--- build-logic/convention/src/main/kotlin/Version.kt
+++ build-logic/convention/src/main/kotlin/Version.kt
@@ -10,7 +10,7 @@ object Version {
     const val compileSdkVersion = 36
     val buildToolsVersion = findBuildToolsVersion()
     const val minSdk = 24
-    const val targetSdk = 35
+    const val targetSdk = 36
     const val versionName = "1.5.8"
 
     private const val defaultNdkVersion = "27.0.12077973"
