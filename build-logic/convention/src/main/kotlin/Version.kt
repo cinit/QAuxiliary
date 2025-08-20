43ca5feb78be67dcb79d8b5b86606001fec3ff96
ACh Sulfate <dex@libart.so>
2025-08-20 20:29:38 +0800

    bump version 1.5.9

--- build-logic/convention/src/main/kotlin/Version.kt
+++ build-logic/convention/src/main/kotlin/Version.kt
@@ -11,7 +11,7 @@ object Version {
     val buildToolsVersion = findBuildToolsVersion()
     const val minSdk = 24
     const val targetSdk = 36
-    const val versionName = "1.5.8"
+    const val versionName = "1.5.9"
 
     private const val defaultNdkVersion = "27.0.12077973"
 
