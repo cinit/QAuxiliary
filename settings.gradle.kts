403e0a5b66563fe7db26f67f367d24d6511babb5
ACh Sulfate <dex@libart.so>
2025-10-13 20:06:26 +0800

    chore: update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -50,7 +50,7 @@ dependencyResolutionManagement {
 includeBuild("build-logic")
 
 plugins {
-    id("com.gradle.develocity") version "4.2"
+    id("com.gradle.develocity") version "4.2.1"
     id("org.gradle.toolchains.foojay-resolver-convention") version ("1.0.0")
 }
 
