fe888ce07f85d920866870b154694d6f74cef099
mmmpipi <153883613+mmmpipi@users.noreply.github.com>
2026-03-31 20:14:31 +0800

    feat: add HideQZoneAD on QQnt

--- app/src/main/java/io/github/relimus/hook/HideQZoneAD.kt
+++ app/src/main/java/io/github/relimus/hook/HideQZoneAD.kt
@@ -22,13 +22,19 @@
 
 package io.github.relimus.hook
 
+import android.content.Context
+import android.view.View
+import com.github.kyuubiran.ezxhelper.utils.findConstructorOrNull
 import com.github.kyuubiran.ezxhelper.utils.findMethod
+import com.github.kyuubiran.ezxhelper.utils.hookAfter
 import com.github.kyuubiran.ezxhelper.utils.hookBefore
 import io.github.qauxv.base.annotation.FunctionHookEntry
 import io.github.qauxv.base.annotation.UiItemAgentEntry
 import io.github.qauxv.dsl.FunctionEntryRouter
 import io.github.qauxv.hook.CommonSwitchFunctionHook
 import io.github.qauxv.util.Initiator
+import io.github.qauxv.util.QQVersion
+import io.github.qauxv.util.requireMinQQVersion
 import io.github.qauxv.util.TIMVersion
 import io.github.qauxv.util.requireMinTimVersion
 import xyz.nextalone.util.throwOrTrue
@@ -37,15 +43,39 @@ import xyz.nextalone.util.throwOrTrue
 @UiItemAgentEntry
 object HideQZoneAD : CommonSwitchFunctionHook() {
     override val name = "隐藏QQ空间广告"
-    override val description = "仅在 TIM_NT 上测试通过"
+    override val description = "隐藏QQ空间和好友动态里的广告"
     override val uiItemLocation = FunctionEntryRouter.Locations.Simplify.MAIN_UI_MISC
-    override val isAvailable = requireMinTimVersion(TIMVersion.TIM_4_0_95_BETA)
+    override val isAvailable = requireMinQQVersion(QQVersion.QQ_8_9_0) || requireMinTimVersion(TIMVersion.TIM_4_0_95_BETA)
 
     override fun initOnce() = throwOrTrue {
-        Initiator.loadClass("com.qzone.proxy.feedcomponent.model.gdt.QZoneAdFeedDataExtKt").findMethod {
-            name == "isShowingRecommendAd"
-        }.hookBefore {
-            it.result = true
+        if (requireMinQQVersion(QQVersion.QQ_8_9_0)) {
+            listOf(
+                "com.qzone.reborn.feedpro.widget.comment.QZoneFeedProDetailBottomAdBlockView", // 说说详情页广告
+                "com.qzone.reborn.feedpro.itemview.ad.card.QZoneCardAdFeedProItemView",
+                "com.qzone.reborn.feedpro.itemview.ad.card.QZoneCardMultiPicAdFeedProItemView",
+                "com.qzone.reborn.feedpro.itemview.ad.carousel.QZoneCarouselCardVideoAdFeedProItemView",
+                "com.qzone.reborn.feedpro.itemview.ad.contract.QZoneContractCardAdFeedProItemView",
+                "com.qzone.reborn.feedpro.itemview.ad.contract.QZoneContractFullFrameAdFeedProItemView",
+                "com.qzone.reborn.feedx.itemview.ad.QZoneAdVideoFeedItemView", // 9.2.5 样式(好友动态)
+                "com.qzone.reborn.feedx.itemview.ad.QZoneAdRewardFeedItemView",
+                "com.qzone.reborn.feedx.itemview.ad.QZoneAdPictureFeedItemView",
+                "com.qzone.reborn.feedx.itemview.ad.QZoneAdMDPAFeedItemView"
+            ).forEach { className ->
+                Initiator.load(className)?.findConstructorOrNull {
+                    parameterTypes[0] == Context::class.java
+                }?.hookAfter { param ->
+                    val view = param.thisObject as View
+                    view.visibility = View.GONE
+                    view.layoutParams.height = 0
+                    view.layoutParams.width = 0
+                }
+            }
+        }else{
+            Initiator.loadClass("com.qzone.proxy.feedcomponent.model.gdt.QZoneAdFeedDataExtKt").findMethod {
+                name == "isShowingRecommendAd"
+            }.hookBefore {
+                it.result = true
+            }
         }
     }
 }
